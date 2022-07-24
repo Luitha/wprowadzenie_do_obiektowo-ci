344c90a618c515db538bdc5d45ed324fd82ca92d
Luitha <bardat.87@gmail.com>
2022-07-24 11:20:39 +0200

    Tworzenie kalkulatora

--- src/Main.java
+++ src/Main.java
@@ -22,5 +22,7 @@ public class Main {
         System.out.println("Proszę podać drugą liczbę");
         int secondVariable = scanner.nextInt();
         System.out.println("Twój wynik to " + calculator.add(firstVariable, secondVariable));
+        System.out.println("Twój wynik to " + calculator.sub(firstVariable, secondVariable));
+        System.out.println("Twój wynik to " + calculator.multiply(firstVariable, secondVariable));
     }
 }
